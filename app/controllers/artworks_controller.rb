c37a3c094ded15128f059bbe64fb091d6b073ea6
Loren Cipriano <loren25cipriano@gmail.com>
2021-08-23 16:51:40 -0700

    add seed data

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -5,15 +5,19 @@ class ArtworksController < ApplicationController
   def index
     @artworks = Artwork.all
 
+    # render json: @artworks
   end
 
   # GET /artworks/1 or /artworks/1.json
   def show
+    # render json: @artwork
   end
 
   # GET /artworks/new
   def new
     @artwork = Artwork.new
+
+    # render json: @artwork
   end
 
   # GET /artworks/1/edit
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# User.create(username: "admin", password: "password")
+
+Artwork.create(title: "test", 
+    description: "this is a test seed", 
+    img: "https://appdevelopermagazine.com/scripts/resize/?path=/images/news_images/Making-Application-Testing-a-First-Class-Citizen-App-Developer-Magazine_gd5jd6gn.jpg&width=700",
+    price: 0.99
+)
+
