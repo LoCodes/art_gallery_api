9781405301ef6de3ff334f4b0147f602f6816658
Loren Cipriano <loren25cipriano@gmail.com>
2021-08-24 21:27:03 -0700

    add skip_before_action in artworkscontroller to fix post fetch from client

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,5 +1,7 @@
 class ArtworksController < ApplicationController
   before_action :set_artwork, only: %i[ show edit update destroy ]
+  skip_before_action :verify_authenticity_token
+
 
   # GET /artworks or /artworks.json
   def index
